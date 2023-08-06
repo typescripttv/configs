b55bced29a536bea9c702571be5710a14b3c5627
Benny Neugebauer <mail@bennycode.com>
2023-08-06 23:43:46 +0200

    refactor: Replace "no-duplicate-imports" with "import/no-duplicates"

--- packages/tsconfig-common/index.ts
+++ packages/tsconfig-common/index.ts
@@ -1,5 +1,13 @@
 import {myFunction} from './myFunction.js';
 
+import {SomeValue, type SomeType} from './type-import.js';
+
+console.log(SomeValue);
+
+export function logInput(input: SomeType) {
+  console.log(input);
+}
+
 await Promise.resolve();
 
 console.log(myFunction(100, 200));
---
+++ packages/tsconfig-common/type-import.ts
@@ -0,0 +1,7 @@
+export type SomeType = {
+  abc: string;
+  xyz: string;
+}
+
+export const SomeValue = 1337;
+
