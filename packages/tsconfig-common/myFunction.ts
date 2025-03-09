698226b25bf3ae2986897ca055bdbe872dc465ad
Benny Neugebauer <mail@bennycode.com>
2025-03-09 09:46:04 +0100

    refactor: Use TypeScript 5.8

--- packages/tsconfig-common/myFunction.ts
+++ packages/tsconfig-common/myFunction.ts
@@ -1,3 +1,8 @@
+// Testing "Import Assertions"
+// @see https://typescript.tv/new-features/what-are-ecmascript-modules/
+import user from './user.json' assert {type: 'json'};
+
 export function myFunction(a: number, b: number) {
+  console.log(user.age);
   return a + b;
 }
